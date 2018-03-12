346da564483ee659131889d94388cb16a15d9c6e
rackelhahn69 <31383702+rackelhahn69@users.noreply.github.com>
2018-03-12 18:41:29 +0100

    dies, das

--- read_data.R
+++ read_data.R
@@ -2,7 +2,8 @@ setwd("~/Documents/Uni/criminology_rational_choice")
 
 library(tidyverse)
 library(magrittr)
-library(haven)
+
+library(labelled)
 
 rckl <- 
   haven::read_dta("./data/Becker_Mehlkop.dta")
@@ -15,3 +16,23 @@ tibble(varn =
          unlist()
        ) %>% 
   filter(str_detect(labl, regex("schwarzfahr", ignore_case=TRUE)))
+
+rckl %>% 
+  transmute(
+    hndl_absi = V107,
+    kost_wert = V103,
+    kost_prob = V104,
+    nutz_wert = V105,
+    nutz_prob = V106
+  ) %T>% 
+  {print(var_label(.))} %T>% 
+  {print(val_labels(.))} %>% 
+  mutate(
+    hndl_absi = factor(hndl_absi, 1:2, labels=c("nein", "ja")),
+    kost_wert = factor(kost_wert, 1:5, ordered=TRUE),
+    kost_prob = factor(kost_prob, 1:5, ordered=TRUE),
+    nutz_wert = factor(nutz_wert, 1:5, ordered=TRUE),
+    nutz_prob = factor(nutz_prob, 1:5, ordered=TRUE)
+  ) %>% 
+  mutate_if(is.ordered, . %>% as.integer()) %>% 
+  na.omit()
---
+++ versicherungsbetrug.R
@@ -0,0 +1,110 @@
+setwd("~/Documents/Uni/criminology_rational_choice")
+
+library(tidyverse)
+library(magrittr)
+
+library(labelled)
+
+library(mgcv)
+
+theme_bw() %+replace%
+  theme(panel.border=element_blank(),
+        panel.grid=element_blank(),
+        plot.title=element_text(size=12)) %>% 
+  theme_set()
+
+####
+####
+####
+
+rawd <- 
+  haven::read_dta("./data/Becker_Mehlkop.dta")
+
+tibble(varn = 
+         colnames(rawd),
+       labl = 
+         labelled::var_label(rawd) %>% 
+         map(. %>% {if_else(is.null(.), NA_character_, .)}) %>% 
+         unlist()
+       ) %>% 
+  filter(str_detect(labl, regex("schwarzfahr", ignore_case=TRUE)))
+
+rckl <- 
+  rawd %>% 
+  select(str_c("V", 103:107)) %>% 
+  set_names(c("kost_wert", "kost_prob",
+              "nutz_wert", "nutz_prob",
+              "hndl_absi")) %T>% 
+  {print(var_label(.))} %T>% 
+  {print(val_labels(.))} %>% 
+  mutate(
+    kost_wert = factor(kost_wert, 1:5, ordered=TRUE),
+    kost_prob = factor(kost_prob, 1:5, ordered=TRUE),
+    nutz_wert = factor(nutz_wert, 1:5, ordered=TRUE),
+    nutz_prob = factor(nutz_prob, 1:5, ordered=TRUE),
+    hndl_absi = factor(hndl_absi, 1:2, labels=c("nein", "ja"))
+  ) %>% 
+  mutate_if(is.ordered, . %>% as.integer()) %>%
+  na.omit()
+
+# form <-
+#   hndl_absi ~ 
+#   s(nutz_wert, by=nutz_prob, k=5, bs="cr") + 
+#   # s(nutz_prob, k=5, bs="cr") + 
+#   s(kost_wert, by=kost_prob, k=5, bs="cr")# + 
+#   # s(kost_prob, k=5, bs="cr")
+
+form <-
+  hndl_absi ~ 
+    s(nutz_wert, k=5, bs="cr") * 
+    s(nutz_prob, k=5, bs="cr") + 
+    s(kost_wert, k=5, bs="cr") * 
+    s(kost_prob, k=5, bs="cr")
+
+form <-
+  hndl_absi ~
+  s(nutz_prob, by=nutz_wert, k=5, bs="cr") +
+  # s(nutz_prob, k=5, bs="cr") +
+  s(kost_prob, by=kost_wert, k=5, bs="cr")# +
+  # s(kost_prob, k=5, bs="cr")
+
+modl <- 
+  gam(form, data=rckl, family="binomial") %T>% 
+  {print(summary(.))}
+
+plot_nutz <- 
+  modl %>% 
+  visreg::visreg2d("nutz_prob", "nutz_wert", plot=FALSE) %$%
+  as_tibble(z) %>% 
+  rownames_to_column("x") %>% 
+  gather(key="y", value="z", -x) %>% 
+  mutate(x = as.integer(x),
+         y = y %>% str_replace("V", "") %>% as.integer()) %T>%
+  {assign("maxz", max(abs(.$z)), pos=1)} %>% 
+  ggplot(aes(x=x, y=y, group=z, fill=z)) +
+  geom_bin2d() +
+  scale_fill_distiller(type="div", palette="Spectral",
+                       limits=c(-maxz,maxz), direction=1) +
+  labs(title="Nutzen",
+       x="Eintrittswahrscheinlichkeit",
+       y="Wert") +
+  theme(legend.position="none")
+  
+plot_kost <-
+  modl %>% 
+  visreg::visreg2d("kost_prob", "kost_wert", plot=FALSE) %$%
+  as_tibble(z) %>% 
+  rownames_to_column("x") %>% 
+  gather(key="y", value="z", -x) %>% 
+  mutate(x = as.integer(x),
+         y = y %>% str_replace("V", "") %>% as.integer()) %T>%
+  {assign("maxz", max(abs(.$z)), pos=1)} %>% 
+  ggplot(aes(x=x, y=y, group=z, fill=z)) +
+  geom_bin2d() +
+  scale_fill_distiller(type="div", palette="Spectral",
+                       limits=c(-maxz,maxz), direction=1) +
+  labs(title="Kosten",
+       x="Eintrittswahrscheinlichkeit",
+       y="Wert")
+
+patchwork::wrap_plots(plot_nutz, plot_kost, nrow=1)
